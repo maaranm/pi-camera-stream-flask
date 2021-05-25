5d17e50e368cb448b77919975127cffbcef097f3
Thamilmaaran Murugathas <maaranmurugathas@crescentschool.org>
2021-05-25 13:50:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def gen(camera):
     #get camera frame
     while True:
         if camera_streaming:
+            print('brooo')
             frame = camera.get_frame()
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
