1c1db66d664ae7e091739fdb732f385c0be2c046
Thamilmaaran Murugathas <maaranmurugathas@crescentschool.org>
2021-05-25 14:26:42 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,23 +1,40 @@
 import cv2
+from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self):
-        self.vs = cv2.VideoCapture(0)
+    def __init__(self, type):
+        self.cam_type = type
+        if self.cam_type == 'pi':
+            self.vs = PiVideoStream().start()
+        else:
+            self.vs = cv2.VideoCapture(0)
         time.sleep(2.0)
 
     def __del__(self):
-        self.vs.stop()
+        if self.cam_type == 'pi':
+            self.vs.stop()
+        else:
+            self.vs.release()
 
     def release(self):
-    	self.vs.release()
+    	if self.cam_type == 'pi':
+            self.vs.stop()
+        else:
+            self.vs.release()
 
     def start(self):
-    	self.vs = cv2.VideoCapture(0)
+        if self.cam_type == 'pi':
+            self.vs = PiVideoStream().start()
+        else:
+    	    self.vs = cv2.VideoCapture(0)
 
     def get_frame(self):
-        _, frame = self.vs.read()
+        if self.cam_type == 'pi':
+            frame = self.vs.read()
+        else:
+            _, frame = self.vs.read()
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
