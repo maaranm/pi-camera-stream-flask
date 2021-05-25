aa6d52b7a103b05924e51a0e0067df6a6267cd31
Thamilmaaran Murugathas <maaranmurugathas@crescentschool.org>
2021-05-25 12:19:24 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,25 +1,15 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This scrtipt script..
-
 import cv2
 import imutils
 import time
 import numpy as np
 
 class VideoCamera(object):
-    def __init__(self, flip = False):
+    def __init__(self):
         self.vs = cv2.VideoCapture(0)
-        self.flip = flip
         time.sleep(2.0)
 
     def __del__(self):
         self.vs.stop()
-
-    def flip_if_needed(self, frame):
-        if self.flip:
-            return np.flip(frame, 0)
-        return frame
 
     def get_frame(self):
         _, frame = self.vs.read()
