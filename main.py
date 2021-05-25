285fb040d443ff26c015f7c8c6414211f316b4d8
Thamilmaaran Murugathas <maaranmurugathas@crescentschool.org>
2021-05-25 13:24:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ webcam = VideoCamera()
 # App Globals (do not edit)
 app = Flask(__name__)
 
+camera_streaming = True
+
 @app.route('/')
 def index():
     return render_template('index.html') #you can customze index.html here
 
 def gen(camera):
     #get camera frame
-    while True:
+    while camera_streaming:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
@@ -25,9 +27,19 @@ def video_feed():
     return Response(gen(webcam),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/modify_feed', methods=['POST'])
+def modify_feed():
+    if not request.json:
+        abort(400)
+    camera_streaming = request.json('isStreaming')
+    if camera_streaming:
+        camera.start()
+    else:
+        camera.release()
+    
+
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0', debug=False)
-    
 
 
